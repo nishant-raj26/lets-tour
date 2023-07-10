09571c4143c524ac2649f2e53134359ab35b3927
Nishant <Nishantraj87726@gmail.com>
2023-07-10 13:24:45 +0530

    1st commit

---
+++ app/components/Loading.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Loading = () => {
+  return (
+    <div className="h-screen bg-slate-300 flex justify-center items-center text-3xl " >
+      <div className="animate-bounce transition ease-out h-24 " >Loading....</div>
+    </div>
+  );
+};
+
+export default Loading;
